73107130037cecbe4f51b77c64723db806488d1e
Shivan Taher <shi.taher@gmail.com>
2025-05-17 07:37:25 +0200

    Initial commit: LilyGo T5 2.13 inch e-paper PlatformIO project with RGB LED and U8g2 support

---
+++ include/GxEPD_BitmapExamples.h
@@ -0,0 +1,8 @@
+#ifndef _GxEPD_BitmapExamples_H_
+#define _GxEPD_BitmapExamples_H_
+
+#include <GxGDGDEW0102T4/BitmapExamples.h>
+
+#define GxEPD_BitmapExamplesQ "BitmapExamples/GxGDGDEW0102T4"
+
+#endif 
