6524454be6e21d78f55257377df591c013b893fc
Shivan Taher <shi.taher@gmail.com>
2025-05-27 17:27:24 +0200

    Update AIWeatherPrompt to enhance prompt generation style
    - Modified the prompt generation to include an additional style option for summarizing weather data in a funny way.
    - Improved formatting by adding newline characters for better readability.

--- src/AIWeatherPrompt.cpp
+++ src/AIWeatherPrompt.cpp
@@ -5,8 +5,9 @@ AIWeatherPrompt::AIWeatherPrompt() {
 
 String AIWeatherPrompt::generatePrompt(const Weather& weather) {
     String prompt = "I will share a JSON payload with you from the Open Meteo API which has weather forecast data for the current day. ";
-    prompt += "You have to summarize it into one sentence in the following style: ";
-    prompt += "- a bizarre conspiracy theory describing the weather ";
+    prompt += "You have to summarize it into one sentence in the following style:\n";
+    prompt += "- a bizarre conspiracy theory describing the weather\n";
+    prompt += "- a funny way\n";
     prompt += "- a short sentence\n";
     prompt += "Weather data: ";
     prompt += weather.getLastPayload();
