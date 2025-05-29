1b6f863009dd2721948b07da43559e72c92377ca
Shivan Taher <shi.taher@gmail.com>
2025-05-29 16:38:48 +0200

    Refactor AIWeatherPrompt to streamline prompt options
    - Removed the options for generating prompts in a funny way and using simple words, focusing on a more concise and direct approach to weather summary generation.

--- src/AIWeatherPrompt.cpp
+++ src/AIWeatherPrompt.cpp
@@ -7,10 +7,8 @@ String AIWeatherPrompt::generatePrompt(const Weather& weather) {
     String prompt = "I will share a JSON payload with you from the Open Meteo API which has weather forecast data for the current day. ";
     prompt += "You have to summarize it into one sentence in the following style:\n";
     prompt += "- a bizarre conspiracy theory describing the weather\n";
-    prompt += "- a funny way\n";
     prompt += "- a short sentence that is 18 words or less\n";
     prompt += "- include the actual temperature in the sentence\n";
-    prompt += "- use simple words\n";
     prompt += "- make sure the weather forecast is included in the sentence\n";
     prompt += "Weather data: ";
     prompt += weather.getLastPayload();
