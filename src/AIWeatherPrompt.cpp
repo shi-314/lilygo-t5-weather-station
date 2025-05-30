916a00135c3641d4280371ab2e6511823b9190b4
Shivan Taher <shi.taher@gmail.com>
2025-05-30 11:42:23 +0200

    Refactor AIWeatherPrompt to clarify guidelines for generating weather summaries, emphasizing brevity and the inclusion of current and forecasted weather without referencing location or inappropriate content.

--- src/AIWeatherPrompt.cpp
+++ src/AIWeatherPrompt.cpp
@@ -6,16 +6,12 @@ AIWeatherPrompt::AIWeatherPrompt() {
 String AIWeatherPrompt::generatePrompt(const Weather& weather) {
     String prompt = "I will share a JSON payload with you from the Open Meteo API which has weather forecast data for the current day. ";
     prompt += "You have to summarize it into one sentence in the following style:\n";
-    // prompt += "- a bizarre conspiracy theory describing the weather\n";
-    // prompt += "- make sure the sentence is very hilarious\n";
     prompt += "- a short sentence that is 18 words or less\n";
     prompt += "- include the rough temperature in the sentence\n";
-    prompt += "- make sure the weather forecast is included in the sentence\n";
-    // prompt += "- consider using recent conspiracy theories to make the sentence more interesting\n";
-    // prompt += "- the conspiracy theory should be about the weather and somewhat plausible\n";
-    prompt += "- you can the location and culture but don't mention the location\n";
+    prompt += "- make sure the weather forecast for the whole day is included in the sentence\n";
     prompt += "- you can use the time of the day to make the sentence more interesting, but don't mention the exact time\n";
-    // prompt += "- don't hesitate to be inappropriate\n";
+    prompt += "- don't mention the location\n";
+    prompt += "- only include the current weather and the forecast for the remaining day, not the past\n";
     prompt += "Weather data: ";
     prompt += weather.getLastPayload();
     
